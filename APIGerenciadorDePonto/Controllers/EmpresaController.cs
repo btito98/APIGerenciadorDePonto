9ceed58bd913f7cd302d0950686265b94beddb52
Gabriel Souza Batista <gabriel.batista@suporterenova.onmicrosoft.com>
2023-11-16 13:50:23 -0300

    atualizando controller

--- APIGerenciadorDePonto/Controllers/EmpresaController.cs
+++ APIGerenciadorDePonto/Controllers/EmpresaController.cs
@@ -1,4 +1,5 @@
-﻿using GrenciadorDePonto.Application.DTOs;
+﻿using APIGerenciadorDePonto.Model;
+using GrenciadorDePonto.Application.DTOs;
 using GrenciadorDePonto.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,7 +9,7 @@ namespace GernciadorDePontoAPI.Controllers
     [ApiController]
     public class EmpresaController : ControllerBase
     {
-        private readonly IEmpresaService _empresaService;
+        private readonly IServiceAsync<Empresa, EmpresaDTO> _empresaService;
         public EmpresaController(IEmpresaService empresaService)
         {
             _empresaService = empresaService;
@@ -29,19 +30,19 @@ namespace GernciadorDePontoAPI.Controllers
             }
         }
 
-        //[HttpPost("AdicionarEmpresa")]
-        //public async Task<ActionResult> Post([FromBody] EmpresaDTO empresaDTO)
-        //{
-        //    try
-        //    {
-        //        await _empresaService.AddEmpresa(empresaDTO);
-        //        return Ok("Empresa cadastrada com sucesso");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
+        [HttpPost("AdicionarEmpresa")]
+        public async Task<ActionResult> Post([FromBody] EmpresaDTO empresaDTO)
+        {
+            try
+            {
+                await _empresaService.AddAsync(empresaDTO);
+                return Ok("Empresa cadastrada com sucesso");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
 
 
